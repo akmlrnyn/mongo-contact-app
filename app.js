674bf7816ea42e0f384a2e7cd59e3e70222bf37c
akmlrnyn <akmalranyan@gmail.com>
2023-12-03 14:42:26 +0700

    setup EJS, routes, and mongoose

--- app.js
+++ app.js
@@ -1,2 +1,60 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = 3000;
+
+// Setup EJS
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(express.urlencoded({
+  extended: true,
+}));
+
+// Home page
+app.get('/', (req, res) => {
+  const students = [{
+    name: 'Akmal Faiq',
+    email: 'akmalranyan@gmail.com',
+  },
+  {
+    name: 'Faiq Akmal',
+    email: 'faaiw@gmail.com',
+  },
+  {
+    name: 'Ranyan',
+    email: 'ranyan@gmail.com',
+  },
+  ];
+  res.render('index', {
+    layout: 'layouts/main-layout.ejs',
+    students,
+    nama: 'Akmal',
+    header: 'ExpressJS',
+  });
+});
+
+// About page
+app.get('/about', (req, res) => {
+  res.render('about', {
+    layout: 'layouts/main-layout.ejs',
+    nama: 'Akmal',
+    header: 'ExpressJS - About Page',
+  });
+});
+
+// Contact page
+app.get('/contact', (req, res) => {
+  const contacts = loadContact();
+  res.render('contact', {
+    contacts,
+    layout: 'layouts/main-layout.ejs',
+    nama: 'Akmal',
+    header: 'ExpressJS - Contact Page',
+    msg: req.flash('msg'),
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Mongo Contact App | Listening at http://localhost:${port}`);
+});
