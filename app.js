f348815b2d30f488c8ac36ba0ad14c65b54e288c
akmlrnyn <akmalranyan@gmail.com>
2023-12-04 14:06:40 +0700

    made delete operation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
 const { body, validationResult, check } = require('express-validator');
+const methodOverride = require('method-override');
 
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
@@ -12,6 +13,9 @@ const port = 3000;
 
 require('./utils/db');
 const Contact = require('./model/contact');
+
+// Setup method-override
+app.use(methodOverride('_method'));
 
 // Setup EJS
 app.set('view engine', 'ejs');
@@ -112,6 +116,31 @@ app.post('/contact', [
       res.redirect('/contact');
     });
   }
+});
+
+// Delete contact
+// app.get('/contact/:nama', async (req, res) => {
+//   const contact = await Contact.findOne({ nama: req.params.nama });
+
+//   if (!contact) {
+//     res.status(404);
+//     res.send('<h1>404</h1>');
+//   } else {
+//     Contact.deleteOne({ _id: contact._id }).then((result) => {
+//       // Send Flash Message
+//       req.flash('msg', 'Data berhasil dihapus!');
+//       res.redirect('/contact');
+//     });
+//   }
+// });
+
+// Delete Contact with delete method
+app.delete('/contact', (req, res) => {
+  Contact.deleteOne({ nama: req.body.nama }).then((result) => {
+    // Send Flash Message
+    req.flash('msg', 'Data berhasil dihapus!');
+    res.redirect('/contact');
+  });
 });
 
 // Detail contact
